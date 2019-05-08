eebb4fe45d09433cfdb6d42fd6517f0ce364799d
Bojan Krstic <krstic.bojan80@gmail.com>
2019-05-08 13:51:52 +0200

    Updates

---
+++ precache-manifest.b7078ce78d3b8feeb15e28c1357394b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f7b38d4557cb78aebe2fad2eb90c5b",
+    "url": "/crypto-app-react/index.html"
+  },
+  {
+    "revision": "a92b7aa073a0efd3daee",
+    "url": "/crypto-app-react/static/css/main.6a68655d.chunk.css"
+  },
+  {
+    "revision": "2bb9279153d2af12dc4e",
+    "url": "/crypto-app-react/static/js/2.0f1d4b6d.chunk.js"
+  },
+  {
+    "revision": "a92b7aa073a0efd3daee",
+    "url": "/crypto-app-react/static/js/main.83b1a6d5.chunk.js"
+  },
+  {
+    "revision": "16c5ea89702b0de3ee8b",
+    "url": "/crypto-app-react/static/js/runtime~main.4318fb6f.js"
+  }
+]);
